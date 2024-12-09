5527c432096d150bbcf5be19337206f15d9cffc1
TimurYamchuk <134388477+TimurYamchuk@users.noreply.github.com>
2024-12-10 01:36:05 +0300

    Update MainWindow.xaml.cs

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -23,7 +12,20 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (sender is Button button)
+            {
+                InputField.Text += button.Content.ToString();
+            }
+        }
 
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputField.Text = string.Empty;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Вы ввели: {InputField.Text}", "Ввод завершен", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
